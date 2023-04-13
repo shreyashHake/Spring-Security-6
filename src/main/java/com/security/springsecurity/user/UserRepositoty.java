c7c184c61f38f0ebadf9f38ee8b5b6752e139eb3
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-04-13 11:56:15 +0530

    New files added

---
+++ src/main/java/com/security/springsecurity/user/UserRepositoty.java
@@ -0,0 +1,9 @@
+package com.security.springsecurity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepositoty extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
